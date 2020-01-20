2f551d77b5ad255f8e8da104d2ca7d4d067e2437
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-20 22:34:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  counter = 0
  new_numbers =[]
   while array[counter] do
-    new_numbers.push(array[counter])**2
+    new_numbers[counter] = (array[counter])**2
     counter += 1
   end
   new_numbers
