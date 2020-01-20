5d8990a648e049b23ce72cb293156a28e8d81757
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-20 22:30:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
  counter = 0
- 
+ new_numbers =[]
   while array[counter] do
-    array.each{ |counter| array << i ** 2 }
+    new_numbers.push((array[counter])**)
     counter += 1
   end
+  new_numbers
 end
